c09a41b61fafa8f157410f52b70fee3232fc18f4
emperorpenguin9 <flying.penguin9196@outlook.com>
2024-06-13 19:47:04 -0400

    simple chat pubnub react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,172 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import {
+  Channel,
+  Chat,
+  Message,
+  MixedTextTypedElement,
+  TimetokenUtils,
+  User,
+} from "@pubnub/chat";
+import "./App.css";
 
-function App() {
+const userData = [
+  {
+    id: "support-agent",
+    data: {
+      name: "John (Support Agent)",
+      custom: { initials: "SA", avatar: "#9fa7df" },
+    },
+  },
+  {
+    id: "supported-user",
+    data: {
+      name: "Mary Watson",
+      custom: { initials: "MW", avatar: "#ffab91" },
+    },
+  },
+];
+const randomizedUsers = Math.random() < 0.5 ? userData : userData.reverse();
+
+export default function App() {
+  const [chat, setChat] = useState<Chat>();
+  const [text, setText] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [channel, setChannel] = useState<Channel>();
+  const [messages, setMessages] = useState<Message[]>([]);
+  const messageListRef = useRef<HTMLElement>(null);
+
+  async function handleSend(event: React.SyntheticEvent) {
+    event.preventDefault();
+    if (!text || !channel) return;
+    await channel.sendText(text);
+    setText("");
+  }
+
+  useEffect(() => {
+    if (!messageListRef.current) return;
+    messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+  }, [messages]);
+
+  useEffect(() => {
+    if (!channel) return;
+    return channel.connect((message) =>
+      setMessages((messages) => [...messages, message])
+    );
+  }, [channel]);
+
+  useEffect(() => {
+    async function initalizeChat() {
+      const chat = await Chat.init({
+        publishKey: "pub-c-8eb69f0f-6735-4cf7-ace4-85d66893f770",
+        subscribeKey: "sub-c-433f9e43-e2b4-4ab0-a4ea-0f23ef968bf3",
+        uuid: randomizedUsers[0].id,
+      });
+
+      const currentUser = await chat.currentUser.update(
+        randomizedUsers[0].data
+      );
+      const interlocutor =
+        (await chat.getUser(randomizedUsers[1].id)) ||
+        (await chat.createUser(randomizedUsers[1].id, randomizedUsers[1].data));
+      const { channel } = await chat.createDirectConversation({
+        user: interlocutor,
+        channelData: { name: "Support Channel" },
+      });
+      setChat(chat);
+      setUsers([currentUser, interlocutor]);
+      setChannel(channel);
+    }
+
+    initalizeChat();
+  }, []);
+
+  const renderMessagePart = useCallback(
+    (messagePart: MixedTextTypedElement) => {
+      if (messagePart.type === "text") {
+        return messagePart.content.text;
+      }
+      if (messagePart.type === "plainLink") {
+        return (
+          <a href={messagePart.content.link}>{messagePart.content.link}</a>
+        );
+      }
+      if (messagePart.type === "textLink") {
+        return (
+          <a href={messagePart.content.link}>{messagePart.content.text}</a>
+        );
+      }
+      if (messagePart.type === "mention") {
+        return (
+          <a href={`https://pubnub.com/${messagePart.content.id}`}>
+            {messagePart.content.name}
+          </a>
+        );
+      }
+
+      return "";
+    },
+    []
+  );
+
+  if (!chat || !channel) return <p>Loading...</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <h3>{channel.name}</h3>
+        <h3>{chat.currentUser.name}</h3>
       </header>
-    </div>
+
+      <section className="message-list" ref={messageListRef}>
+        <ol>
+          {messages.map((message) => {
+            const user = users.find((user) => user.id === message.userId);
+            return (
+              <li key={message.timetoken}>
+                <aside style={{ background: String(user?.custom?.avatar) }}>
+                  {user?.custom?.initials}
+                </aside>
+                <article>
+                  <h3>
+                    {user?.name}
+                    <time>
+                      {TimetokenUtils.timetokenToDate(
+                        message.timetoken
+                      ).toLocaleTimeString([], {
+                        timeStyle: "short",
+                      })}
+                    </time>
+                  </h3>
+                  <p>
+                    {message
+                      .getLinkedText()
+                      .map((messagePart: MixedTextTypedElement, i: number) => (
+                        <span key={String(i)}>
+                          {renderMessagePart(messagePart)}
+                        </span>
+                      ))}
+                  </p>
+                </article>
+              </li>
+            );
+          })}
+        </ol>
+      </section>
+
+      <form className="message-input" onSubmit={handleSend}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Send message"
+        />
+        <input
+          type="submit"
+          value="➔"
+          onClick={handleSend}
+          style={{ color: text && "#de2440" }}
+        />
+      </form>
+    </main>
   );
 }
-
-export default App;
